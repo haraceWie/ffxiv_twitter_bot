92ed69c0c4f0c355f778f43446e3ad5601b9e730
harace.wie <dnlrhksgus0511@gmail.com>
2023-02-04 22:25:10 +0900

    feat: login  모듈 추가

--- Main.py
+++ Main.py
@@ -2,6 +2,9 @@
 from flask import Flask, request, send_from_directory, make_response
 from flask_cors import CORS
 from http import HTTPStatus
+import flask_sqlalchemy
+import flask_praetorian
+import flask_cors
 
 import Twitter, hashlib, hmac, base64, os, logging, json
 
@@ -10,6 +13,12 @@ CURRENT_USER_ID = os.environ.get('CURRENT_USER_ID', None)
 	     
 app = Flask(__name__)	
 cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
+app.config['SECRET_KEY'] = 'top secret'
+app.config['JWT_ACCESS_LIFESPAN'] = {'hours': 24}
+app.config['JWT_REFRESH_LIFESPAN'] = {'days': 30}
+
+db = flask_sqlalchemy.SQLAlchemy()
+guard = flask_praetorian.Praetorian()
 
 @app.route('/')
 def default_route():        
@@ -79,6 +88,81 @@ def twitterEventReceived():
     
     return ('', HTTPStatus.OK)
 
+
+
+
+
+# Initialize the flask-praetorian instance for the app
+guard.init_app(app, User)
+
+# Initialize a local database for the example
+app.config['SQLALCHEMY_DATABASE_URI'] = f"sqlite:///{os.path.join(os.getcwd(), 'database.db')}"
+db.init_app(app)
+
+# Initializes CORS so that the api_tool can talk to the example app
+cors.init_app(app)
+
+# Add users for the example
+with app.app_context():
+    db.create_all()
+    if db.session.query(User).filter_by(username='Yasoob').count() < 1:
+        db.session.add(User(
+          username='Yasoob',
+          password=guard.hash_password('strongpassword'),
+          roles='admin'
+            ))
+    db.session.commit()
+
+
+
+  
+@app.route('/api/login', methods=['POST'])
+def login():
+    """
+    Logs a user in by parsing a POST request containing user credentials and
+    issuing a JWT token.
+    .. example::
+       $ curl http://localhost:5000/api/login -X POST \
+         -d '{"username":"Yasoob","password":"strongpassword"}'
+    """
+    req = request.get_json(force=True)
+    username = req.get('username', None)
+    password = req.get('password', None)
+    user = guard.authenticate(username, password)
+    ret = {'access_token': guard.encode_jwt_token(user)}
+    return ret, 200
+
+  
+@app.route('/api/refresh', methods=['POST'])
+def refresh():
+    """
+    Refreshes an existing JWT by creating a new one that is a copy of the old
+    except that it has a refrehsed access expiration.
+    .. example::
+       $ curl http://localhost:5000/api/refresh -X GET \
+         -H "Authorization: Bearer <your_token>"
+    """
+    print("refresh request")
+    old_token = request.get_data()
+    new_token = guard.refresh_jwt_token(old_token)
+    ret = {'access_token': new_token}
+    return ret, 200
+  
+  
+@app.route('/api/protected')
+@flask_praetorian.auth_required
+def protected():
+    """
+    A protected endpoint. The auth_required decorator will require a header
+    containing a valid JWT
+    .. example::
+       $ curl http://localhost:5000/api/protected -X GET \
+         -H "Authorization: Bearer <your_token>"
+    """
+    return {'message': f'protected endpoint (allowed user {flask_praetorian.current_user().username})'}
+
+
+
                 	    
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 65010))
@@ -86,3 +170,8 @@ if __name__ == '__main__':
     app.logger.handlers = gunicorn_logger.handlers
     app.logger.setLevel(gunicorn_logger.level)
     app.run(host='0.0.0.0', port=port, debug=True)
+
+
+
+
+
