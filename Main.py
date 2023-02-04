14a360a60441b6cf08e06c89302e6449b86f8a8c
harace.wie <dnlrhksgus0511@gmail.com>
2023-02-04 22:42:46 +0900

    feat: test

--- Main.py
+++ Main.py
@@ -129,65 +129,65 @@ guard.init_app(app, User)
 # Initialize a local database for the example
 app.config['SQLALCHEMY_DATABASE_URI'] = f"sqlite:///{os.path.join(os.getcwd(), 'database.db')}"
 db.init_app(app)
-# Add users for the example
-with app.app_context():
-    db.create_all()
-    if db.session.query(User).filter_by(username='Yasoob').count() < 1:
-        db.session.add(User(
-          username='Yasoob',
-          password=guard.hash_password('strongpassword'),
-          roles='admin'
-            ))
-    db.session.commit()
+# # Add users for the example
+# with app.app_context():
+#     db.create_all()
+#     if db.session.query(User).filter_by(username='Yasoob').count() < 1:
+#         db.session.add(User(
+#           username='Yasoob',
+#           password=guard.hash_password('strongpassword'),
+#           roles='admin'
+#             ))
+#     db.session.commit()
 
 
 
 
   
-# @app.route('/api/login', methods=['POST'])
-# def login():
-#     """
-#     Logs a user in by parsing a POST request containing user credentials and
-#     issuing a JWT token.
-#     .. example::
-#        $ curl http://localhost:5000/api/login -X POST \
-#          -d '{"username":"Yasoob","password":"strongpassword"}'
-#     """
-#     req = request.get_json(force=True)
-#     username = req.get('username', None)
-#     password = req.get('password', None)
-#     user = guard.authenticate(username, password)
-#     ret = {'access_token': guard.encode_jwt_token(user)}
-#     return ret, 200
+@app.route('/api/login', methods=['POST'])
+def login():
+    """
+    Logs a user in by parsing a POST request containing user credentials and
+    issuing a JWT token.
+    .. example::
+       $ curl http://localhost:5000/api/login -X POST \
+         -d '{"username":"Yasoob","password":"strongpassword"}'
+    """
+    req = request.get_json(force=True)
+    username = req.get('username', None)
+    password = req.get('password', None)
+    user = guard.authenticate(username, password)
+    ret = {'access_token': guard.encode_jwt_token(user)}
+    return ret, 200
 
   
-# @app.route('/api/refresh', methods=['POST'])
-# def refresh():
-#     """
-#     Refreshes an existing JWT by creating a new one that is a copy of the old
-#     except that it has a refrehsed access expiration.
-#     .. example::
-#        $ curl http://localhost:5000/api/refresh -X GET \
-#          -H "Authorization: Bearer <your_token>"
-#     """
-#     print("refresh request")
-#     old_token = request.get_data()
-#     new_token = guard.refresh_jwt_token(old_token)
-#     ret = {'access_token': new_token}
-#     return ret, 200
+@app.route('/api/refresh', methods=['POST'])
+def refresh():
+    """
+    Refreshes an existing JWT by creating a new one that is a copy of the old
+    except that it has a refrehsed access expiration.
+    .. example::
+       $ curl http://localhost:5000/api/refresh -X GET \
+         -H "Authorization: Bearer <your_token>"
+    """
+    print("refresh request")
+    old_token = request.get_data()
+    new_token = guard.refresh_jwt_token(old_token)
+    ret = {'access_token': new_token}
+    return ret, 200
   
   
-# @app.route('/api/protected')
-# @flask_praetorian.auth_required
-# def protected():
-#     """
-#     A protected endpoint. The auth_required decorator will require a header
-#     containing a valid JWT
-#     .. example::
-#        $ curl http://localhost:5000/api/protected -X GET \
-#          -H "Authorization: Bearer <your_token>"
-#     """
-#     return {'message': f'protected endpoint (allowed user {flask_praetorian.current_user().username})'}
+@app.route('/api/protected')
+@flask_praetorian.auth_required
+def protected():
+    """
+    A protected endpoint. The auth_required decorator will require a header
+    containing a valid JWT
+    .. example::
+       $ curl http://localhost:5000/api/protected -X GET \
+         -H "Authorization: Bearer <your_token>"
+    """
+    return {'message': f'protected endpoint (allowed user {flask_praetorian.current_user().username})'}
 
 
 
