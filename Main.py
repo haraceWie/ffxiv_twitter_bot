cbbb8145c22020959d700aba4031aff0002fd0f9
harace.wie <dnlrhksgus0511@gmail.com>
2023-02-04 22:37:55 +0900

    feat: db 연결부

--- Main.py
+++ Main.py
@@ -90,43 +90,43 @@ def twitterEventReceived():
 
 
 
-# # A generic user model that might be used by an app powered by flask-praetorian
-# class User(db.Model):
-#     id = db.Column(db.Integer, primary_key=True)
-#     username = db.Column(db.Text, unique=True)
-#     password = db.Column(db.Text)
-#     roles = db.Column(db.Text)
-#     is_active = db.Column(db.Boolean, default=True, server_default='true')
+# A generic user model that might be used by an app powered by flask-praetorian
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.Text, unique=True)
+    password = db.Column(db.Text)
+    roles = db.Column(db.Text)
+    is_active = db.Column(db.Boolean, default=True, server_default='true')
 
-#     @property
-#     def rolenames(self):
-#         try:
-#             return self.roles.split(',')
-#         except Exception:
-#             return []
+    @property
+    def rolenames(self):
+        try:
+            return self.roles.split(',')
+        except Exception:
+            return []
 
-#     @classmethod
-#     def lookup(cls, username):
-#         return cls.query.filter_by(username=username).one_or_none()
+    @classmethod
+    def lookup(cls, username):
+        return cls.query.filter_by(username=username).one_or_none()
 
-#     @classmethod
-#     def identify(cls, id):
-#         return cls.query.get(id)
+    @classmethod
+    def identify(cls, id):
+        return cls.query.get(id)
 
-#     @property
-#     def identity(self):
-#         return self.id
+    @property
+    def identity(self):
+        return self.id
 
-#     def is_valid(self):
-#         return self.is_active
+    def is_valid(self):
+        return self.is_active
 
 
-# # Initialize the flask-praetorian instance for the app
-# guard.init_app(app, User)
+# Initialize the flask-praetorian instance for the app
+guard.init_app(app, User)
 
-# # Initialize a local database for the example
-# app.config['SQLALCHEMY_DATABASE_URI'] = f"sqlite:///{os.path.join(os.getcwd(), 'database.db')}"
-# db.init_app(app)
+# Initialize a local database for the example
+app.config['SQLALCHEMY_DATABASE_URI'] = f"sqlite:///{os.path.join(os.getcwd(), 'database.db')}"
+db.init_app(app)
 
 # # Add users for the example
 # with app.app_context():
